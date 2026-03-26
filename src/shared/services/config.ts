1ee9addebd3ba139c3a2f48dc242c020a8f6d5d5
Matheus Sena Rosa <matheussenarosa8@gmail.com>
2026-03-26 14:09:40 -0300

    feat: update API base URL to use dynamic protocol and host

--- src/shared/services/config.ts
+++ src/shared/services/config.ts
@@ -7,7 +7,7 @@ type RetryableAxiosRequestConfig = AxiosRequestConfig & {
 };
 
 export const api = axios.create({
-  baseURL: `http://localhost:3000/api`,
+  baseURL: `${window.location.protocol}//${window.location.host}/api`,
   withCredentials: true,
 });
 
